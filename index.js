6393056101b7b5914568a84960a1f4ed0155e03e
Mijan Karim <mkatbd@gmail.com>
2021-03-31 05:18:04 +0600

    connected db

--- index.js
+++ index.js
@@ -1,14 +1,40 @@
-const express = require('express')
-const app = express()
-const port = 5050;
-require('dotenv').config();
+const express = require("express");
+const app = express();
+const MongoClient = require("mongodb").MongoClient;
+const cors = require("cors");
+require("dotenv").config();
+const port = process.env.PORT || 5050;
 
-app.get('/', (req, res) => {
-  res.send('Hello World Server site')
-})
+app.use(cors());
+app.use(express.json());
 
-console.log(process.env.DB_USER);
+app.get("/", (req, res) => {
+  res.send("Hello World Server site");
+});
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cbpdo.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+client.connect((err) => {
+  const productsCollection = client.db("foodMarket").collection("products");
+
+  app.get("/products", (req, res) => {
+     productsCollection.find()
+     .toArray((err, products) => {
+         res.send( products)
+     }) 
+  });
+
+  app.post("/addProduct", (req, res) => {
+    const newProduct = req.body;
+    console.log("adding new product:", newProduct);
+    productsCollection.insertOne(newProduct).then((result) => {
+      console.log("inserted count", result.insertedCount);
+      res.send(result.insertedCount > 0);
+    });
+  });
+});
+
+app.listen(port);
